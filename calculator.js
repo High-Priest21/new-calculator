4e4b2ad144bf0a44fd6c9dc37f2423abc54b567a
High-Priest21 <58502553+High-Priest21@users.noreply.github.com>
2019-12-22 14:35:00 +0100

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+function add(){
+    var a = document.getElementById('firstnumber').value;
+    var b = document.getElementById('secondnumber').value;
+    var c = parseInt(a, 10);
+    var d = parseInt(b, 10);
+    return c + d;
+    }
+
+function multiply(){
+    var a = document.getElementById('firstnumber').value;
+    var b = document.getElementById('secondnumber').value;
+    var c = parseInt(a, 10);
+    var d = parseInt(b, 10);
+    return c * d;
+}
+function divide(){
+    var a = document.getElementById('firstnumber').value;
+    var b = document.getElementById('secondnumber').value;
+    var c = parseInt(a, 10);
+    var d = parseInt(b, 10);
+    return c / d;
+}
